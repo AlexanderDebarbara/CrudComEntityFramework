238adbbf9a79a391d3729ee0d3f57daa47e6eae4
Alexander Debarbara <debarbara4@gmail.com>
2017-04-26 15:00:22 -0400

    BancoDeTalentos
    Crud com Entity Framework

---
+++ BancoDeTalentos/BancoDeTalentos.Entity/BancoDeTalentosContexto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BancoDeTalentos.Entity
+{
+    public class BancoDeTalentosContexto : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var mapCandidato = modelBuilder.Entity<Candidato>();
+            mapCandidato.Property(x => x.IdCandidato)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            mapCandidato.Property(x => x.Nome)
+                .IsVariableLength()
+                .IsRequired()
+                .HasMaxLength(150);
+            mapCandidato.Property(x => x.Cpf)
+                .IsVariableLength()
+                .IsRequired()
+                .HasMaxLength(11);
+            mapCandidato.Property(x => x.Telefone)
+                .IsVariableLength()
+                .HasMaxLength(15)
+                .IsOptional();
+            mapCandidato.Property(x => x.Email)
+                .IsVariableLength()
+                .IsOptional()
+                .HasMaxLength(150);
+            mapCandidato.HasKey(x => x.IdCandidato);
+            mapCandidato.ToTable("Candidato");
+
+                
+        }
+
+        public System.Data.Entity.DbSet<BancoDeTalentos.Candidato> Candidatoes { get; set; }
+    }
+}
---
+++ BancoDeTalentos/BancoDeTalentos.Web/Controllers/CandidatoController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BancoDeTalentos;
+using BancoDeTalentos.Entity;
+
+namespace BancoDeTalentos.Web.Controllers
+{
+    public class CandidatoController : Controller
+    {
+        private BancoDeTalentosContexto db = new BancoDeTalentosContexto();
+
+        // GET: Candidato
+        public ActionResult Index()
+        {
+            return View(db.Candidatoes.ToList());
+        }
+
+        // GET: Candidato/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Candidato candidato = db.Candidatoes.Find(id);
+            if (candidato == null)
+            {
+                return HttpNotFound();
+            }
+            return View(candidato);
+        }
+
+        // GET: Candidato/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Candidato/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdCandidato,Nome,Cpf,Telefone,Email")] Candidato candidato)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Candidatoes.Add(candidato);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(candidato);
+        }
+
+        // GET: Candidato/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Candidato candidato = db.Candidatoes.Find(id);
+            if (candidato == null)
+            {
+                return HttpNotFound();
+            }
+            return View(candidato);
+        }
+
+        // POST: Candidato/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdCandidato,Nome,Cpf,Telefone,Email")] Candidato candidato)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(candidato).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(candidato);
+        }
+
+        // GET: Candidato/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Candidato candidato = db.Candidatoes.Find(id);
+            if (candidato == null)
+            {
+                return HttpNotFound();
+            }
+            return View(candidato);
+        }
+
+        // POST: Candidato/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Candidato candidato = db.Candidatoes.Find(id);
+            db.Candidatoes.Remove(candidato);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
